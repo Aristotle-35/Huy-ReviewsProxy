7f3998191b2bb2790b4be2963ce0bbe49e8f6fc6
hgtruong <huygtruong@gmail.com>
2019-01-07 20:54:04 -0800

    Rendering all four updated components to handle "/1/" case

--- proxy/server.js
+++ proxy/server.js
@@ -4,8 +4,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(/\/\d+\//, express.static(path.join(__dirname, 'public')));
+
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
